aaf7e51f4bedf0947f00638d19650fa89a3a7ee3
Grace Kim <goooyoon@gmail.com>
2020-05-28 06:48:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli)
+
+  if katz_deli.length == 0
+     puts "The line is currently empty."
+   else
+     current_line = []
+     katz_deli.each.with_index(1) do |name,index|
+       current_line << "#{index}. #{name}"
+       end
+     puts "The line is currently: #{current_line.join(" ")}"
+
+
+   end
+ end
